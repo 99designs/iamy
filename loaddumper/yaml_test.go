9730c920403af4fc91b11e6cfa6fe3ea99cfce34
Michael Tibben <michael.tibben@99designs.com>
2015-08-17 23:00:19 +1000

    Add test for yaml

---
+++ loaddumper/yaml_test.go
@@ -0,0 +1,60 @@
+package loaddumper
+
+import (
+	"io/ioutil"
+	"path/filepath"
+	"reflect"
+	"testing"
+)
+import "os"
+
+func readDir(path string) map[string][]byte {
+	files := map[string][]byte{}
+	filepath.Walk(path, func(p string, info os.FileInfo, err error) error {
+		if err != nil {
+			panic(err)
+		}
+		if !info.IsDir() {
+			files[info.Name()], err = ioutil.ReadFile(p)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		return nil
+	})
+
+	return files
+}
+
+func TestRoundTrip(t *testing.T) {
+	d, err := os.Getwd()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	Yaml.Dir = filepath.Join(d, "testdata")
+
+	accountData, err := Yaml.Load()
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	testdir, err := ioutil.TempDir("", "loaddumpertest")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	Yaml.Dir = testdir
+	err = Yaml.Dump(accountData)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	yamlDirData := readDir(Yaml.Dir)
+	testdirData := readDir(testdir)
+	eq := reflect.DeepEqual(yamlDirData, testdirData)
+	if !eq {
+		t.Error("Directory contents are not equal")
+	}
+}
