01cf94bda655fdf95fe19ca4fb58959c48561f43
Michael Tibben <michael.tibben@99designs.com>
2015-09-21 10:20:43 +1000

    Simplify aws api calls to use GetAccountAuthorizationDetails

--- iamy/aws.go
+++ iamy/aws.go
@@ -32,23 +32,148 @@ func (a *awsIamFetcher) Fetch() (*AccountData, error) {
 	}
 	a.account = data.Account
 
-	if data.Users, err = a.loadUsers(); err != nil {
-		return nil, err
-	}
+	complete := false
+	var marker *string = nil
+	for !complete {
+		resp, err := a.client.GetAccountAuthorizationDetails(&iam.GetAccountAuthorizationDetailsInput{
+			MaxItems: aws.Int64(1000),
+			Marker:   marker,
+			Filter: aws.StringSlice([]string{
+				iam.EntityTypeUser,
+				iam.EntityTypeGroup,
+				iam.EntityTypeRole,
+				iam.EntityTypeLocalManagedPolicy,
+			}),
+		})
+		if err != nil {
+			return nil, err
+		}
 
-	if data.Policies, err = a.loadPolicies(); err != nil {
-		return nil, err
-	}
+		err = a.populateData(resp, &data)
+		if err != nil {
+			return nil, err
+		}
 
-	if data.Groups, err = a.loadGroups(); err != nil {
-		return nil, err
-	}
-
-	if data.Roles, err = a.loadRoles(); err != nil {
-		return nil, err
+		if *resp.IsTruncated {
+			marker = resp.Marker
+		} else {
+			complete = true
+		}
 	}
 
 	return &data, nil
+}
+
+func (a *awsIamFetcher) populateInlinePolicies(source []*iam.PolicyDetail, target *[]InlinePolicy) error {
+	for _, ip := range source {
+		doc, err := NewPolicyDocumentFromEncodedJson(*ip.PolicyDocument)
+		if err != nil {
+			return err
+		}
+		*target = append(*target, InlinePolicy{
+			Name:   *ip.PolicyName,
+			Policy: doc,
+		})
+	}
+
+	return nil
+}
+
+func (a *awsIamFetcher) populateData(resp *iam.GetAccountAuthorizationDetailsOutput, data *AccountData) error {
+	for _, userResp := range resp.UserDetailList {
+		if cfnResourceRegexp.MatchString(*userResp.UserName) {
+			logPrintf("Skipping CloudFormation generated user %s", *userResp.UserName)
+			continue
+		}
+
+		user := User{
+			Name: *userResp.UserName,
+			Path: *userResp.Path,
+		}
+
+		for _, g := range userResp.GroupList {
+			user.Groups = append(user.Groups, *g)
+		}
+		for _, p := range userResp.AttachedManagedPolicies {
+			user.Policies = append(user.Policies, a.account.normalisePolicyArn(*p.PolicyArn))
+		}
+		if err := a.populateInlinePolicies(userResp.UserPolicyList, &user.InlinePolicies); err != nil {
+			return err
+		}
+
+		data.Users = append(data.Users, user)
+	}
+
+	for _, groupResp := range resp.GroupDetailList {
+		if cfnResourceRegexp.MatchString(*groupResp.GroupName) {
+			logPrintf("Skipping CloudFormation generated group %s", *groupResp.GroupName)
+			continue
+		}
+
+		group := Group{
+			Name: *groupResp.GroupName,
+			Path: *groupResp.Path,
+		}
+
+		for _, p := range groupResp.AttachedManagedPolicies {
+			group.Policies = append(group.Policies, a.account.normalisePolicyArn(*p.PolicyArn))
+		}
+		if err := a.populateInlinePolicies(groupResp.GroupPolicyList, &group.InlinePolicies); err != nil {
+			return err
+		}
+
+		data.Groups = append(data.Groups, group)
+	}
+
+	for _, roleResp := range resp.RoleDetailList {
+		if cfnResourceRegexp.MatchString(*roleResp.RoleName) {
+			logPrintf("Skipping CloudFormation generated role %s", *roleResp.RoleName)
+			continue
+		}
+
+		role := Role{
+			Name: *roleResp.RoleName,
+			Path: *roleResp.Path,
+		}
+
+		var err error
+		role.AssumeRolePolicyDocument, err = NewPolicyDocumentFromEncodedJson(*roleResp.AssumeRolePolicyDocument)
+		if err != nil {
+			return err
+		}
+		for _, p := range roleResp.AttachedManagedPolicies {
+			role.Policies = append(role.Policies, a.account.normalisePolicyArn(*p.PolicyArn))
+		}
+		if err := a.populateInlinePolicies(roleResp.RolePolicyList, &role.InlinePolicies); err != nil {
+			return err
+		}
+
+		data.Roles = append(data.Roles, role)
+	}
+
+	for _, policyResp := range resp.Policies {
+		if cfnResourceRegexp.MatchString(*policyResp.PolicyName) {
+			logPrintf("Skipping CloudFormation generated policy %s", *policyResp.PolicyName)
+			continue
+		}
+
+		for _, version := range policyResp.PolicyVersionList {
+			if *version.IsDefaultVersion {
+				doc, err := NewPolicyDocumentFromEncodedJson(*version.Document)
+				if err != nil {
+					return err
+				}
+
+				data.Policies = append(data.Policies, Policy{
+					Name:   *policyResp.PolicyName,
+					Path:   *policyResp.Path,
+					Policy: doc,
+				})
+			}
+		}
+	}
+
+	return nil
 }
 
 func (a *awsIamFetcher) getAccount() (*Account, error) {
@@ -137,315 +262,4 @@ func determineAccountIdViaDefaultSecurityGroup() (string, error) {
 	}
 
 	return *sg.SecurityGroups[0].OwnerId, nil
-}
-
-func (a *awsIamFetcher) loadUsers() ([]User, error) {
-	logPrintln("Fetching AWS IAM users")
-
-	resp, err := a.client.ListUsers(&iam.ListUsersInput{})
-	if err != nil {
-		return nil, err
-	}
-
-	users := []User{}
-
-	for _, user := range resp.Users {
-		if cfnResourceRegexp.MatchString(*user.UserName) {
-			logPrintf("Skipping CloudFormation generated user %s", *user.UserName)
-			continue
-		}
-
-		logPrintf("Fetching %s", *user.Arn)
-
-		u := User{
-			Name: *user.UserName,
-			Path: *user.Path,
-		}
-
-		if err = a.populateUserGroups(&u); err != nil {
-			return nil, err
-		}
-
-		if err = a.populateUserPolicies(&u); err != nil {
-			return nil, err
-		}
-
-		users = append(users, u)
-	}
-
-	return users, nil
-}
-
-func (a *awsIamFetcher) populateUserGroups(user *User) error {
-	params := &iam.ListGroupsForUserInput{
-		UserName: aws.String(user.Name), // Required
-	}
-
-	user.Groups = []string{}
-	resp, err := a.client.ListGroupsForUser(params)
-	if err != nil {
-		return err
-	}
-
-	for _, group := range resp.Groups {
-		user.Groups = append(user.Groups, *group.GroupName)
-	}
-
-	return nil
-}
-
-func (a *awsIamFetcher) populateUserPolicies(user *User) error {
-	params := &iam.ListUserPoliciesInput{
-		UserName: aws.String(user.Name), // Required
-	}
-
-	user.InlinePolicies = []InlinePolicy{}
-	resp, err := a.client.ListUserPolicies(params)
-	if err != nil {
-		return err
-	}
-
-	for _, policyName := range resp.PolicyNames {
-		policyResp, err := a.client.GetUserPolicy(&iam.GetUserPolicyInput{
-			PolicyName: policyName,
-			UserName:   aws.String(user.Name),
-		})
-		if err != nil {
-			return err
-		}
-
-		doc, err := NewPolicyDocumentFromEncodedJson(*policyResp.PolicyDocument)
-		if err != nil {
-			return err
-		}
-
-		user.InlinePolicies = append(user.InlinePolicies, InlinePolicy{
-			Name:   *policyName,
-			Policy: doc,
-		})
-	}
-
-	user.Policies = []string{}
-	attachedResp, err := a.client.ListAttachedUserPolicies(&iam.ListAttachedUserPoliciesInput{
-		UserName: aws.String(user.Name),
-	})
-
-	for _, policyResp := range attachedResp.AttachedPolicies {
-		user.Policies = append(user.Policies, a.account.normalisePolicyArn(*policyResp.PolicyArn))
-	}
-
-	return nil
-}
-
-func (a *awsIamFetcher) loadPolicies() ([]Policy, error) {
-	logPrintln("Fetching AWS IAM policies")
-
-	resp, err := a.client.ListPolicies(&iam.ListPoliciesInput{
-		Scope:        aws.String(iam.PolicyScopeTypeLocal),
-		OnlyAttached: aws.Bool(false),
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	policies := []Policy{}
-
-	for _, respPolicy := range resp.Policies {
-		if cfnResourceRegexp.MatchString(*respPolicy.PolicyName) {
-			logPrintf("Skipping CloudFormation generated policy %s", *respPolicy.PolicyName)
-			continue
-		}
-
-		logPrintf("Fetching policy %s", *respPolicy.Arn)
-
-		respVersions, err := a.client.ListPolicyVersions(&iam.ListPolicyVersionsInput{
-			PolicyArn: respPolicy.Arn,
-		})
-		if err != nil {
-			return nil, err
-		}
-
-		for _, version := range respVersions.Versions {
-			if *version.IsDefaultVersion {
-				respPolicyVersion, err := a.client.GetPolicyVersion(&iam.GetPolicyVersionInput{
-					PolicyArn: respPolicy.Arn,
-					VersionId: version.VersionId,
-				})
-				if err != nil {
-					return nil, err
-				}
-				doc, err := NewPolicyDocumentFromEncodedJson(*respPolicyVersion.PolicyVersion.Document)
-				if err != nil {
-					return nil, err
-				}
-				policy := Policy{
-					Name:   *respPolicy.PolicyName,
-					Path:   *respPolicy.Path,
-					Policy: doc,
-				}
-
-				policies = append(policies, policy)
-			}
-		}
-	}
-
-	return policies, nil
-}
-
-func (a *awsIamFetcher) loadGroups() ([]Group, error) {
-	logPrintln("Fetching AWS IAM groups")
-
-	params := &iam.ListGroupsInput{}
-	resp, err := a.client.ListGroups(params)
-	if err != nil {
-		return nil, err
-	}
-
-	groups := []Group{}
-
-	for _, groupResp := range resp.Groups {
-		if cfnResourceRegexp.MatchString(*groupResp.GroupName) {
-			logPrintf("Skipping CloudFormation generated group %s", *groupResp.GroupName)
-			continue
-		}
-
-		logPrintf("Fetching group %s", *groupResp.Arn)
-		group := Group{
-			Name: *groupResp.GroupName,
-			Path: *groupResp.Path,
-		}
-
-		if err = a.populateGroupPolicies(&group); err != nil {
-			return nil, err
-		}
-
-		groups = append(groups, group)
-	}
-
-	return groups, nil
-}
-
-func (a *awsIamFetcher) populateGroupPolicies(group *Group) error {
-	params := &iam.ListGroupPoliciesInput{
-		GroupName: aws.String(group.Name),
-	}
-
-	group.InlinePolicies = []InlinePolicy{}
-	resp, err := a.client.ListGroupPolicies(params)
-	if err != nil {
-		return err
-	}
-
-	for _, policyName := range resp.PolicyNames {
-		policyResp, err := a.client.GetGroupPolicy(&iam.GetGroupPolicyInput{
-			PolicyName: policyName,
-			GroupName:  aws.String(group.Name),
-		})
-		if err != nil {
-			return err
-		}
-
-		doc, err := NewPolicyDocumentFromEncodedJson(*policyResp.PolicyDocument)
-		if err != nil {
-			return err
-		}
-
-		group.InlinePolicies = append(group.InlinePolicies, InlinePolicy{
-			Name:   *policyName,
-			Policy: doc,
-		})
-	}
-
-	group.Policies = []string{}
-	attachedResp, err := a.client.ListAttachedGroupPolicies(&iam.ListAttachedGroupPoliciesInput{
-		GroupName: aws.String(group.Name),
-	})
-
-	for _, policyResp := range attachedResp.AttachedPolicies {
-		group.Policies = append(group.Policies, a.account.normalisePolicyArn(*policyResp.PolicyArn))
-	}
-
-	return nil
-}
-
-func (a *awsIamFetcher) loadRoles() ([]Role, error) {
-	logPrintln("Fetching AWS IAM Roles")
-
-	resp, err := a.client.ListRoles(&iam.ListRolesInput{})
-	if err != nil {
-		return nil, err
-	}
-
-	roles := []Role{}
-
-	for _, roleResp := range resp.Roles {
-		if cfnResourceRegexp.MatchString(*roleResp.RoleName) {
-			logPrintf("Skipping CloudFormation generated role %s", *roleResp.RoleName)
-			continue
-		}
-
-		logPrintf("Fetching role %s", *roleResp.Arn)
-
-		doc, err := NewPolicyDocumentFromEncodedJson(*roleResp.AssumeRolePolicyDocument)
-		if err != nil {
-			return nil, err
-		}
-
-		role := Role{
-			Name: *roleResp.RoleName,
-			Path: *roleResp.Path,
-			AssumeRolePolicyDocument: doc,
-		}
-
-		if err = a.populateRolePolicies(&role); err != nil {
-			return nil, err
-		}
-
-		roles = append(roles, role)
-	}
-
-	return roles, nil
-}
-
-func (a *awsIamFetcher) populateRolePolicies(role *Role) error {
-	params := &iam.ListRolePoliciesInput{
-		RoleName: aws.String(role.Name),
-	}
-
-	role.InlinePolicies = []InlinePolicy{}
-	resp, err := a.client.ListRolePolicies(params)
-	if err != nil {
-		return err
-	}
-
-	for _, policyName := range resp.PolicyNames {
-		policyResp, err := a.client.GetRolePolicy(&iam.GetRolePolicyInput{
-			PolicyName: policyName,
-			RoleName:   aws.String(role.Name),
-		})
-		if err != nil {
-			return err
-		}
-
-		doc, err := NewPolicyDocumentFromEncodedJson(*policyResp.PolicyDocument)
-		if err != nil {
-			return err
-		}
-
-		role.InlinePolicies = append(role.InlinePolicies, InlinePolicy{
-			Name:   *policyName,
-			Policy: doc,
-		})
-	}
-
-	role.Policies = []string{}
-	attachedResp, err := a.client.ListAttachedRolePolicies(&iam.ListAttachedRolePoliciesInput{
-		RoleName: aws.String(role.Name),
-	})
-
-	for _, policyResp := range attachedResp.AttachedPolicies {
-		role.Policies = append(role.Policies, a.account.normalisePolicyArn(*policyResp.PolicyArn))
-	}
-
-	return nil
 }
