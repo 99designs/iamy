695f43502c9afd54170fd52709cb5d0d4446edc7
Paul Annesley <paul@annesley.cc>
2018-08-21 10:41:30 +1000

    Use STS GetCallerIdentity to find AWS Account ID.
        DEBUG 2018/08/21 10:40:51 Finding AWS account ID via GetCallerIdentity
        DEBUG 2018/08/21 10:40:52 AWS account ID: 123412341234

--- iamy/awsaccountid.go
+++ iamy/awsaccountid.go
@@ -9,13 +9,21 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/aws/aws-sdk-go/service/sts"
 )
 
 // GetAwsAccountId determines the AWS account id associated
 // with the given session
 func GetAwsAccountId(sess *session.Session, debug *log.Logger) (string, error) {
+	debug.Println("Finding AWS account ID via GetCallerIdentity")
+	accountid, err := determineAccountIdViaGetCallerIdentity(sess)
+	if err == nil {
+		debug.Println("AWS account ID:", accountid)
+		return accountid, nil
+	}
+
 	debug.Println("Finding AWS account ID via GetUser")
-	accountid, err := determineAccountIdViaGetUser(sess)
+	accountid, err = determineAccountIdViaGetUser(sess)
 	if err == nil {
 		debug.Println("AWS account ID:", accountid)
 		return accountid, nil
@@ -41,6 +49,15 @@ func GetAwsAccountId(sess *session.Session, debug *log.Logger) (string, error) {
 func getAccountIdFromArn(arn string) string {
 	s := strings.Split(arn, ":")
 	return s[4]
+}
+
+// https://docs.aws.amazon.com/STS/latest/APIReference/API_GetCallerIdentity.html
+func determineAccountIdViaGetCallerIdentity(sess *session.Session) (string, error) {
+	resp, err := sts.New(sess).GetCallerIdentity(&sts.GetCallerIdentityInput{})
+	if err != nil {
+		return "", err
+	}
+	return *resp.Account, nil
 }
 
 // see http://stackoverflow.com/a/18124234
